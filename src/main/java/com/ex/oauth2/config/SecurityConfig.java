721e70713bd2c0b2ac6375e4e9e8a7ade18e0014
zmgu <manggu94@gmail.com>
2024-02-01 18:36:06 +0900

    코드 정리

--- src/main/java/com/ex/oauth2/config/SecurityConfig.java
+++ src/main/java/com/ex/oauth2/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,12 +25,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf((csrf) -> csrf.disable());
-
-        http
-                .formLogin((login) -> login.disable());
-
-        http
+                .csrf((csrf) -> csrf.disable())
+                .formLogin((login) -> login.disable())
                 .httpBasic((basic) -> basic.disable());
 
         http
