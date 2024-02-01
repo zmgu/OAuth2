2149cfc19c55d931cca6f02157e4768a72afbb46
zmgu <manggu94@gmail.com>
2024-02-01 18:33:25 +0900

    authorizedClientService 추가

--- src/main/java/com/ex/oauth2/config/SecurityConfig.java
+++ src/main/java/com/ex/oauth2/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.ex.oauth2.config;
 
 import com.ex.oauth2.oauth2.CustomClientRegistrationRepo;
+import com.ex.oauth2.oauth2.CustomOAuth2AuthorizedClientService;
 import com.ex.oauth2.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,25 +19,32 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepo customClientRegistrationRepo;
+    private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf((csrf) -> csrf.disable())
-                .formLogin((login) -> login.disable())
+                .csrf((csrf) -> csrf.disable());
+
+        http
+                .formLogin((login) -> login.disable());
+
+        http
                 .httpBasic((basic) -> basic.disable());
 
         http
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
                         .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
-                        .userInfoEndpoint(userInfoEndpointConfig ->
+                        .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.clientRegistrationRepository()))
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService)));
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .requestMatchers("/", "/login/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated());
 
         return http.build();
